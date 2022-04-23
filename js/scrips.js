0ece0e0b1a61dd965f635d809c6a335c4db85cf4
erick260392 <erickliconamorales2016@gmail.com>
2022-04-22 19:22:22 -0500

    cambios de portafolio

---
+++ js/scrips.js
@@ -0,0 +1,22 @@
+let toogleTheme = document.getElementById("toogle-theme");
+let toogleIcon = document.getElementById("toogle-icon");
+let toogleText = document.getElementById("toogle-text");
+let toogleColors =document.getElementById("toogle-colors");
+let rootStyles =document.documentElement.style;
+
+
+toogleTheme.addEventListener("click",() =>{
+    document.body.classList.toggle("dark")
+    if(toogleIcon.src.includes("moon.svg")){
+        toogleIcon.src="/portfolio-youtube-inicial/assets/icons/sun.svg"
+        toogleText.textContent="Light Mode"
+    }else{
+        toogleIcon.src="/portfolio-youtube-inicial/assets/icons/moon.svg"
+        toogleText.textContent="Dark Mode"
+    }
+});
+
+toogleColors.addEventListener("click",(e) => {
+   rootStyles.setProperty('--primary-color',e.target.dataset.color);
+});
+
